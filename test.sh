86747e93870f00118733b9e40d299bdeb15c0142
schoutem <110914895+schoutem@users.noreply.github.com>
2024-05-29 22:18:22 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -10,6 +10,14 @@ sleep 1
 sudo apt update -y
 sudo apt upgrade -y
 
+# install software
+echo ""
+echo "install packages"
+sleep 1
+sudo apt install nala -y
+sleep 1
+sudo nala install mc curl apt-transport-https ntp nano software-properties-common -y
+
 # set time
 sleep 1
 echo ""
