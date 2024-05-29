bc47bb9f69f9d13224da865963ecfca025e84a3a
schoutem <110914895+schoutem@users.noreply.github.com>
2024-05-29 22:10:59 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-echo "Update install"
-sudo apt update -y
-sudo apt upgrade -y
-
 sleep 1
 echo ""
 echo "Ok here we go...."
+
+# update and upgrade
+echo "Update and upgrade"
+sleep 1
+sudo apt update -y
+sudo apt upgrade -y
+
+# install software
+echo ""
+echo "install packages"
+sleep 1
+sudo apt install nala -y
+sleep 1
+sudo nala install mc curl apt-transport-https ntp nano software-properties-common -y
