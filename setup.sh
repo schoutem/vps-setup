ee2356431528f1a77a33002343ab94ea1561839a
schoutem <110914895+schoutem@users.noreply.github.com>
2024-07-07 14:26:35 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -39,22 +39,44 @@ if [ "$(id -u)" != "0" ]; then
 fi
 }
 
-check_os(){
-. /etc/os-release
-echo "Check system..."
-echo
-sleep 2
-if [ $ID == 'ubuntu' ]; then
-echo
-echo "OS Ubuntu, ok"
-echo
-lsb_release -a
-else
+
 echo
 echo "This script works best on Ubuntu OS"
-exit 1
+echo "check..."
+echo
+progress_bar 5
+
+#Check Linux Dist
+
+DISTREL="Ubuntu"
+DISTVERSION="24"
+
+source /etc/lsb-release
+
+if [ -z $DISTRIB_RELEASE ] || [ -z $DISTRIB_ID ]; then
+    echo "DISTRIB_RELEASE and DISTRIB_ID are not set"
+else
+    if [ $DISTRIB_ID == "$DISTREL" ]; then
+        IFS='.' read -r -a distro_vers <<< $DISTRIB_RELEASE
+        major_ver=${distro_vers[0]}
+        echo "Distro major version $major_ver"
+        if [ -z $major_ver ]; then
+            echo "Major release version parsing failed"
+        else
+            if [ $(($major_ver)) -ge $DISTVERSION ]; then
+               echo -e "${GREEN}Ubuntu major version is greater than $DISTVERSION, ok!${ENDCOLOR}";
+                # do your operation here
+            else
+                echo -e "${RED}Ubuntu major version is lesser than $DISTVERSION, ok!${ENDCOLOR}";
+                exit
+            fi
+        fi
+    else
+        echo -e "${RED}It is not a Ubuntu Linux but a $DISTREL linux${ENDCOLOR}";
+        exit
+    fi
 fi
-}
+#End Check Linux Dist
 
 ##################################################################################################
 
