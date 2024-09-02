9c4072b47bb71c7231295ed74576d42c19c04ada
schoutem <110914895+schoutem@users.noreply.github.com>
2024-09-02 10:45:52 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,91 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # setup.sh
 # Copyright 2024 schoutem. All rights reserved.
 
-RED="\e[31m"
-GREEN="\e[32m"
-BLUE="\e[34m"
-ENDCOLOR="\e[0m"
-
-# Function to display the progress bar
-progress_bar() {
-  local duration=${1}
-    
-  already_done() { for ((done=0; done<$1; done++)); do printf "#"; done }
-  remaining() { for ((remain=$1; remain<$duration; remain++)); do printf " "; done }
-  percentage() { printf "| %s%%" $(( (($1)*100)/($duration)*100/100 )); }
-  clean_line() { printf "\r"; }
- 
-  for (( current_duration=1; current_duration<=$duration; current_duration++ )); do
-    already_done $current_duration
-    remaining $current_duration
-    percentage $current_duration
-    clean_line
-    sleep 1
-  done
- 
-  clean_line
-}
-
-#Check Linux Dist
-check_root() {
-echo "Check root..."
-sleep 2
-if [ "$(id -u)" != "0" ]; then
-  echo
-	echo -e "${RED}You must be root to execute the script. Exiting....${ENDCOLOR}"
-	exit 1
- else
- echo
- 	echo -e "${GREEN}Yes you are root....${ENDCOLOR}"
-  echo
-fi
-}
-#End Check Linux Dist
-
-
-#Function nench test
-ntest () {
-function nenchtest() {
-    while true; do
-        echo
-        read -p "Do you want test your system with VPS benchmark script? (YES/NO/CANCEL or y/n/c)" yn
-        case $yn in
-            [Yy]* ) return 0;;
-            [Nn]* ) return 1;;
-            [Cc]* ) exit;;
-            * ) echo "Please answer YES, NO, or CANCEL.";;
-        esac
-    done
-}
-
-if nenchtest; then
-    echo -e "${GREEN}Start testing system....${ENDCOLOR}"
-    echo
-	progress_bar 5
-	(curl -s wget.racing/nench.sh | bash; curl -s wget.racing/nench.sh | bash) 2>&1 | tee nench.log
-    echo
- read -p "Press enter to continue"
-else
-   echo
-   echo -e "${RED}Aborting test...${ENDCOLOR}"
-   read -p "Press enter to continue"
-fi
-}
-
-#Check Linux Dist
-
-check_os()
-{
-hostnamectl
-}
-
-
-#End Check Linux Dist
-
-##################################################################################################
-
-echo -e "
+function header_info {
+clear
+cat <<"EOF"
 __      _______   _____    _____      _               
 \ \    / /  __ \ / ____|  / ____|    | |              
  \ \  / /| |__) | (___   | (___   ___| |_ _   _ _ __  
@@ -94,258 +13,188 @@ __      _______   _____    _____      _
     \/   |_|    |_____/  |_____/ \___|\__|\__,_| .__/ 
                                                | |    
                                                |_|    
-" 
+EOF
+}
 
-check_root
+echo -e "\n Loading..."
+
+YW=$(echo "\033[33m")
+BL=$(echo "\033[36m")
+RD=$(echo "\033[01;31m")
+BGN=$(echo "\033[4;92m")
+GN=$(echo "\033[1;92m")
+DGN=$(echo "\033[32m")
+CL=$(echo "\033[m")
+BFR="\\r\\033[K"
+HOLD=" "
+CM="${GREEN}>>${CL}"
+CROSS="${RED}X${CL}"
+
+set -e
+
+#Function to display the progress bar
+function pb {
+# Usage:
+# pb <length> <percentage> <title>
+#
+# Run a simple "echo" command any time to leave the progress bar
+
+declare -i length=$1
+declare -i percent=$2
+title=$3
+declare -i pop=length*percent/100
+declare -i empty=length-pop
+printf "$title ["
+for (( i=0; i<=pop; i++ ))
+do
+    printf "#"
+done
+for (( i=0; i<empty; i++ ))
+do
+    printf " "
+done
+printf "]"
+echo -n " $percent%"
+printf "\r"
+}
+
+function moment () {
+
+function commands {
+sleep 1
+}
+
+pb 30 30 Waiting
+commands > /dev/null 2>&1
+pb 30 60 Waiting
+commands > /dev/null 2>&1
+pb 30 90 Waiting
+commands > /dev/null 2>&1
+pb 30 100 Waiting
+echo
+}
+
+msg_ok() {
+  printf "\e[?25h"
+  local msg="$1"
+  echo
+  echo -e "${BFR} ${CM} ${GN}${msg}${CL}"
+  echo
+}
+
+# This function displays a error message with a red color.
+msg_error() {
+  printf "\e[?25h"
+  local msg="$1"
+  echo
+  echo -e "${BFR} ${CROSS} ${RD}${msg}${CL}"
+  echo
+}
+
+# This function displays an informational message with a yellow color.
+msg_info() {
+  local msg="$1"
+  echo -ne " ${HOLD} ${YW}${msg}${CL}"
+}
+
+# This function displays continue
+function cont {
+echo
+read -p " Press enter to continue..."
+echo
+}
+
+#checkroot
+function check_root() {
+echo "Check root..."
 sleep 2
-echo
-echo "This script works best on Ubuntu/Debian OS"
-echo "check..."
-echo
+if [[ "$(id -u)" -ne 0 || $(ps -o comm= -p $PPID) == "sudo" ]]; then
+  msg_error "You must be root to execute the script. Exiting...."
+	exit 1
+ else
+ echo
+msg_ok "Yes you are root...."
+  echo
+fi
+}
+# End checkroot
 
-progress_bar 5
 
-check_os
+function check_os()
+{
+hostnamectl
+}
 
-##################################################################################################            
+function nenchtest() {
 
-ntest
+echo -e  "${BFR} ${CM} ${GN}Start testing system....${CL}"
+    echo
+		(curl -s wget.racing/nench.sh | bash; curl -s wget.racing/nench.sh | bash) 2>&1 | tee nench.log
+   sleep 2
+   echo
+   cont
+}
 
-#Swapfile
 
-#Check swap excist
+function checkapp () {
+
+ msg_ok "Installing required apps..."
+  
+apt-get install whiptail mc curl apt-transport-https nano software-properties-common systemd-timesyncd -y
+
+msg_info "Update and upgrading your system..."
+
+apt-get update &&  apt-get upgrade -y
+   
+   msg_ok "Done..."
+}
+
+
+function swapset () {
+# does the swap file already exist?
 grep -q "swapfile" /etc/fstab
 
 # if not then create it
 if [ $? -ne 0 ]; then
-  echo
-        echo -e "${GREEN}No swapfile found, ok${ENDCOLOR}" ;
-	echo
+	echo 'swapfile not found. Adding swapfile.'
+	fallocate -l ${swapsize}M /swapfile
+	chmod 600 /swapfile
+	mkswap /swapfile
+	swapon /swapfile
+	echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+else
+	echo 'swapfile found. No changes made.'
 fi
 
-#swapfile set
-# Get total available memory in bytes
-total_memory=$(grep MemTotal /proc/meminfo | awk '{print $2 * 1024}')
- 
-# Function to create and enable swap file
-  create_swap() {
-  local size=$1
-  local swapfile="/swapfile"
- 
-  # Create swap file with the specified size
-   fallocate -l "$size" "$swapfile"
- 
-  # Set permissions
-   chmod 600 "$swapfile"
- 
-  # Make it a swap file
-   mkswap "$swapfile"
- 
-  # Enable swap
-   swapon "$swapfile"
- 
-  # Add swap file to /etc/fstab for auto-mount
-  echo "$swapfile none swap sw 0 0" | tee -a /etc/fstab
- 
-  echo "Swap file created and enabled successfully."
+# output results to terminal
+swapon --show
+cat /proc/swaps
+cat /proc/meminfo | grep Swap
+
+msg_ok "Swapsize set done..."
+
 }
- 
-echo "Choose an option:"
-echo "1. Double the system memory"
-echo "2. Set custom size (in GB)"
-echo "3. None"
- 
-read -p "Enter your choice (1/2/3): " choice
- 
-case $choice in
-  1)
-    # Double the system memory
-    swap_size=$((total_memory * 2))
-    ;;
-  2)
-    read -p "Enter custom size in **GB**: " custom_size
-    # Convert to bytes
-    swap_size=$((custom_size * 1024 * 1024 * 1024))
-    ;;
-  3)
-    echo "Exiting..."
-     ;;
-  *)
-    echo "Invalid option, exiting..."
-    exit 1
-    ;;
-esac
-#End setting swapfile
- 	
-#Specify settings
-echo "Which port do you want to use for SHH?"
-read -r selport
-echo "Entered port: $selport"
+#end swap
 
-PS3="Choose your packet manager (APT or NALA): "
-echo 
-select pm in "apt" "nala"; do
-  echo
-  echo "Choosen manager ${pm}"
-  echo
-  break
-done
-#read -p pm
-
-
-echo "Your settings are: port $selport, packetmanager $pm"
-echo
-read -p "Press enter to continue"
-
-sleep 2
-echo
-echo "Ok here we go...."
-
-#Start set and install
-
-##############################################################################################
-
-# update and upgrade
-echo "Update and upgrade your system..."
-progress_bar 5
-apt update -y
-apt upgrade -y
-
-# install software
-echo
-echo "install packages"
-sleep 1
-if [ "$pm" = "nala" ] ; then
-	echo "Installing nala...."
-	 apt install nala -y
-fi
-sleep 1
- $pm install mc curl apt-transport-https nano software-properties-common -y
-
-#Set Nano settings
-sleep 1
-echo
-echo "Set Nano config..."
-echo
-
+function nanoset () {
 sed -i 's/[# ]*set tabsize 8/set tabsize 4/g' /etc/nanorc
 sed -i -e '/^\(#\|\) set constantshow/s/^.*$/set constantshow/' /etc/nanorc
 sed -i -e '/^\(#\|\) set linenumbers/s/^.*$/set linenumbers/' /etc/nanorc
 sed -i -e '/^\(#\|\) set mouse/s/^.*$/set mouse/' /etc/nanorc
-
+msg_ok "Nano set done..."
 sleep 1
-echo
-echo "Set Nano config done..."
-echo
-#End set Nano settings
-
-#Create swapfile
-echo "Create swapfile"
-create_swap "$swap_size"
-progress_bar 5
-echo "Done..."
-echo
-# output results to terminal
-cat /proc/swaps
-cat /proc/meminfo | grep Swap
-#End create swapfile
-
-#Set swapiness
-function swness() {
-
-# if not then create it
-if [ $? -ne 0 ]; then
-        echo
-        echo -e "${GREEN}Swapiness settings are $var (not set to default), ok${ENDCOLOR}" ;
-	      echo 
-        sleep 1
-        echo -e "${GREEN}No changes necessary${ENDCOLOR}" ;
-        echo
-else
-
-echo "Choose an option:"
-echo "1. Set Swapiness to 10"
-echo "2. Set custom size (10-40)"
-echo "3. None"
- 
-read -p "Enter your choice (1/2/3): " choice
-
-case $choice in
-  1)
-    # Double the system memory
-    swap_size=10
-    ;;
-  2)
-    read -p "Enter custom size in (10-40): " swap_size
-    ;;
-  3)
-    echo "Exiting..."
-    ;;
-  *)
-    echo "Invalid option, exiting..."
-    exit 1
-    ;;
-esac
-
-        echo
-        echo -e "${BLUE}Chance Swapiness to $swap_size...${ENDCOLOR}" ;
-        echo
-        sed -i -e '$a\'$'\n''vm.swappiness = '$swap_size'' /etc/sysctl.conf
-        echo   
-         sysctl -p    
-        sleep 2
-        echo -e "${GREEN}Swapiness settings are set to $swap_size....${ENDCOLOR}" ;
-        echo
-
-fi
-
 }
-#END Set swapiness
 
-#check Swapiness excist
-if [ $(grep -ic "60" /proc/sys/vm/swappiness) -eq 1 ]; then
-    echo -e "${RED}We have found custom swap.${ENDCOLOR}"
-    rmswness
-elif [ $(grep -ic "vm.swappiness" /etc/sysctl.conf) -eq 1 ]; then
-    echo -e "${RED}We have found custom swap in /etc/sysctl.conf.${ENDCOLOR}"
-    rmswness
-else
-    echo -e "${GREEN}We haven't found custom swapiness.${ENDCOLOR}"
-    swness
-fi
-#END check Swapiness excist
-
-# set time
-apt install systemd-timesyncd
-sleep 1
-
-
-
-progress_bar 5
-
-sleep 1
-
+function timeset () {
 #NTP server set
 dpkg-reconfigure tzdata
-echo
-
-progress_bar 5
-
-echo ""
-echo "Check..."
 timedatectl set-ntp true
-sleep 1
-echo ""
-echo "Status NTP Check..."
-echo
-
-progress_bar 5
 timedatectl
+msg_ok "NTP set done..."
+}
 
-sleep 1
+function setssh () {
 
-#set SSH
-echo "Set SHH..."
 sed -i 's/[#]*PermitRootLogin yes/PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config
 sed -i -e '/^\(#\|\)Port 22/s/^.*$/Port '$selport'/' /etc/ssh/sshd_config
 sed -i -e '/^\(#\|\)PasswordAuthentication/s/^.*$/PasswordAuthentication no/' /etc/ssh/sshd_config
@@ -354,24 +203,13 @@ sed -i -e '/^\(#\|\)KbdInteractiveAuthentication/s/^.*$/KbdInteractiveAuthentica
 sed -i -e '/^\(#\|\)ChallengeResponseAuthentication/s/^.*$/ChallengeResponseAuthentication no/' /etc/ssh/sshd_config
 sed -i -e '/^\(#\|\)X11Forwarding/s/^.*$/X11Forwarding no/' /etc/ssh/sshd_config
 sed -i -e '/^\(#\|\)AuthorizedKeysFile/s/^.*$/AuthorizedKeysFile .ssh\/authorized_keys/' /etc/ssh/sshd_config
-sleep 1
-echo
-echo "Restart SSH..."
-progress_bar 5
- sshd -t
 
- systemctl restart ssh
-echo
-echo "Ready installing, SHH port is set to $selport and choosen packetmanager is $pm"
-progress_bar 5
+msg_ok "SSH config set done ..."
+}
 
-echo
-echo "Setting auto updates..."
-echo
-progress_bar 5
+function autoupdate () {
 
-#auto updates
- apt install unattended-upgrades -y &&  apt install update-notifier-common -y
+apt install unattended-upgrades update-notifier-common -y
 
 #50auto-upgrades
 sed -i 's/\/\/	 "${distro_id}:${distro_codename}";/	 "${distro_id}:${distro_codename}";/g' /etc/apt/apt.conf.d/50unattended-upgrades
@@ -390,46 +228,20 @@ sed -i -e '$a\'$'\n''APT::Periodic::AutocleanInterval "7";' /etc/apt/apt.conf.d/
 sed -i 's/\/\/APT::Periodic::Update-Package-Lists "0";/APT::Periodic::Update-Package-Lists "1";/g' /etc/apt/apt.conf.d/20auto-upgrades
 sed -i 's/\/\/APT::Periodic::Unattended-Upgrade "0";/APT::Periodic::Unattended-Upgrade "1";/g' /etc/apt/apt.conf.d/20auto-upgrades
 
-sleep 1
-echo
-echo -e "${BLUE}Now we are going reconfigue unattended service, next step click Enter for Yes.${ENDCOLOR}"
-echo
-sleep 1
 read -p "Press enter to begin reconfigure..."
  dpkg-reconfigure -plow unattended-upgrades
-
-sleep 1
-echo
-echo "service unattended restart..."
-progress_bar 5
- systemctl restart unattended-upgrades.service
-sleep 1
-
-#status Unatended
-echo
-echo "Status test...."
-progress_bar 5
-systemctl status unattended-upgrades
-
+ 
 sleep 1
 echo
 echo "Test it Out with a Dry Run..."
 echo
-progress_bar 5
+
 unattended-upgrades --dry-run --debug
 
-echo
-echo "Ready install unattended-upgrades.."
-echo
-#End auto updates
+msg_ok "Auto update set done ..."
+}
 
-#Firewall set
-
-echo
-echo "Set firewall..."
-
-
-progress_bar 5	
+function setfirewall () {
 
 apt install ufw -y
 
@@ -441,7 +253,6 @@ sleep 1
 ufw default allow outgoing
 sleep 1
 ufw default deny outgoing
-
 
 sleep 1
 
@@ -464,28 +275,90 @@ sudo ufw allow 443
 sleep 1
 ufw enable
 ufw status verbose
-echo
-echo "Set firewall done..."
-#End Firewall set
 
-#Function reboot
-function confirm() {
-    while true; do
-        read -p "Do you want to reboot system? (YES/NO/CANCEL or y/n/c)" yn
-        case $yn in
-            [Yy]* ) return 0;;
-            [Nn]* ) return 1;;
-            [Cc]* ) exit;;
-            * ) echo "Please answer YES, NO, or CANCEL.";;
-        esac
-    done
+msg_ok "Firewall set done ..."
+sleep 1
 }
 
-if confirm; then
-    echo -e "${GREEN}Reboot system....${ENDCOLOR}"
-    echo
-	progress_bar 5
-	 reboot
+#######################################################################################
+
+header_info
+sleep 1
+check_root
+sleep 1
+
+checkapp
+
+msg_ok "This script works best on Ubuntu/Debian OS"
+
+check_os
+
+if whiptail --title "Benchmark test" --yesno "Do you want test your system with VPS benchmark script? (https://github.com/n-st/nench)" 10 100; then
+
+nenchtest
+
 else
-   echo -e "${RED}Aborting the reboot...${ENDCOLOR}"
+echo "Cancel Benchmark test, start setup settings....."
+
+sleep 2
+fi
+
+selport=$(whiptail --inputbox "Please enter your SSH port (example: 223 or 2355)" 10 100 3>&1 1>&2 2>&3)
+
+swapsize=$(whiptail --title "Swapfile set" --menu "Choose an option" 18 100 10 \
+  "512" "set to 512 MB size." \
+  "1024" "set to 1024 MB size." \
+  "2048" "set to 2048 MB size." \
+  "4096" "set to 4096 MB size." \
+  "6144" "set to 6144 MB size." 3>&1 1>&2 2>&3)
+
+if [ -z "$swapsize" ]; then
+  echo "No option was chosen (user hit Cancel)"
+  echo
+else
+echo   "Swapsize $swapsize set"
+
+fi
+
+if whiptail --yesno "Your settings: SSH port: $selport, Swapsize: $swapsize \n\nCancel setup, select No." 10 100; then
+msg_info "Start setup....."
+moment
+else
+echo "Cancel setup....."
+moment
+  clear
+  exit
+fi
+
+swapset
+
+sleep 1
+
+nanoset
+
+sleep 1
+
+timeset
+
+sleep 1
+
+setssh
+
+sleep 1
+
+autoupdate
+
+sleep 1
+
+setfirewall
+
+if whiptail --title "Reboot" --yesno "VPS Setup setup ready, required reboot system?" 10 100; then
+
+msg_ok "Roboot system ..."
+moment
+sshd -t
+sleep 1
+reboot
+else
+cont
 fi
